e2f9af71aae764bff40eb4243432808fa6a43e04
Facundo Rodriguez <facundo.m.rodriguez1993@gmail.com>
2025-03-31 15:06:58 -0300

    new student entities, persistence, service

---
+++ SpringMicroservice/microservice-student/src/main/java/com/microservice/student/persistence/IStudentRepository.java
@@ -0,0 +1,16 @@
+package com.microservice.student.persistence;
+
+import com.microservice.student.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IStudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("SELECT s FROM Student WHERE s.courseId = :idCourse")
+    List<Student> findAllByCourseId(Long idCourse);
+
+}
---
+++ SpringMicroservice/microservice-student/src/main/java/com/microservice/student/service/serviceImpl/StudentServiceImpl.java
@@ -0,0 +1,36 @@
+package com.microservice.student.service.serviceImpl;
+
+import com.microservice.student.entities.Student;
+import com.microservice.student.persistence.IStudentRepository;
+import com.microservice.student.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements IStudentService {
+
+    @Autowired
+    private IStudentRepository studentRepository;
+
+    @Override
+    public List<Student> findAll() {
+        return  studentRepository.findAll();
+    }
+
+    @Override
+    public Student findById(Long id) {
+        return studentRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public void save(Student student) {
+        studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> findByIdCourse(Long idCourse) {
+        return studentRepository.findAllByCourseId(idCourse);
+    }
+}
